3b2ce3678bbb8bd61adcb7b05c2f401794dd78bc
Rinat <bbywpo@gmail.com>
2025-03-10 13:49:24 +0200

    Updated proj.ps1 and added schedule.html

--- proj.ps1
+++ proj.ps1
@@ -17,22 +17,43 @@ if (-not $Day -or -not $Time -or -not $Subject) {
 
 # Create a schedule if all parameters are provided
 $Schedule = @{}
+
 if (-not $Schedule.ContainsKey($Day)) {
     $Schedule[$Day] = @{}
 }
+
 $Schedule[$Day][$Time] = $Subject
 
 # Generate the HTML output
-$html = "<html><head><title>Schedule</title></head><body><h1>Class Schedule</h1><table border='1'><tr><th>Day</th><th>Time</th><th>Subject</th></tr>"
+$html = @"
+<html>
+<head>
+    <title>Schedule</title>
+</head>
+<body>
+    <h1>Class Schedule</h1>
+    <table border='1'>
+        <tr><th>Day</th><th>Time</th><th>Subject</th></tr>
+"@
 
 foreach ($Day in $Schedule.Keys) {
     foreach ($Time in $Schedule[$Day].Keys) {
-        $html += "<tr><td>$Day</td><td>$Time</td><td>$($Schedule[$Day][$Time])</td></tr>"
+        $html += "<tr><td>$Day</td><td>$Time</td><td>$($Schedule[$Day][$Time])</td></tr>`n"
     }
 }
 
-$html += "</table></body></html>"
+$html += @"
+    </table>
+</body>
+</html>
+"@
 
 # Save the schedule to HTML file
 $html | Out-File -FilePath "output\schedule.html"
+
 Write-Output "Schedule saved to output\schedule.html"
+
+# Display the content of schedule.html in Jenkins Console
+Write-Output "======= Schedule HTML Output ======="
+Get-Content "output\schedule.html"
+Write-Output "===================================="
